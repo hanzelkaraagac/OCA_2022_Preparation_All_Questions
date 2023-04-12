e2e5f23ec4f89b4972d5a64eb2ddd9171f4310c4
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-12 19:46:35 +0300

    0CA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q008/Q008.java
@@ -0,0 +1,26 @@
+package oca_2022.q008;
+
+public class Q008 {
+
+	public static void main(String[] args) {	
+		String [] [] arr = {{"A", "B", "C"}, {"D", "E"}};	
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j]+ " " );
+				if (arr[i][j].equals("B")) {
+					continue; //bir etkisi yok
+				}
+			}
+		continue;
+		}	
+	}
+}
+//What is the result? 
+//A.    A B C
+//B.	A B C D E
+//C.	A B D E
+//D.	Compilation fails.
+
+
+//continue; cannot be used outside of a loop
+//break; can be used outside of a loop
