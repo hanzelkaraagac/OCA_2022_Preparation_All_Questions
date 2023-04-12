107fa81739d94c6faeb3a24a15b6889cdb8a8910
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-12 16:02:28 +0300

    0CA 2022 Preparation Section All Questions and Answers

--- src/main/java/oca_2022/q005/Q005.java
+++ src/main/java/oca_2022/q005/Q005.java
@@ -14,5 +14,15 @@ public class Q005 {
             Switch ifadesinde herbir case bloğunun sonunda bulunan break ifadesi isteğe bağlıdır.
             Break ifadesi olmadan, switch ifadesi sonuna kadar gerçekleşir veya
             bir break ifadesi ile karşılaşana kadar sonraki case etiketine devam eder.
+
+
+            The correct answer is B.
+            The break statement, at the end of each case block, is optional in a switch statement.
+            Without a break statement,execution will continue to the next case label until a break is encountered or the end of the switch statement is reached.
+
+            To clarify the other options:
+            A. It is not mandatory to include the default section in a switch statement, but it is often recommended to handle unexpected input.
+            C. The case label literals are fixed and cannot be changed at runtime.
+            D. The expression in a switch statement typically evaluates to a single value, not a collection of values.
     */
 }
