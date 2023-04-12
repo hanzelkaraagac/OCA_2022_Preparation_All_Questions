cf97b38252f6055ce2c446b9cda0a94162726d81
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-12 15:55:13 +0300

    0CA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q005/Q005.java
@@ -0,0 +1,18 @@
+package oca_2022.q005;
+
+public class Q005 {
+
+    /*
+            Which statement is true about the switch statement?
+            A. It must contain the default section.
+            B. The break statement, at the end of each case block, is optional.
+            C. Its case label literals can be changed at runtime.
+            D. Its expression must evaluate to a collection of values.
+
+
+            Switch ifadesiyle ilgili doğru olan ifade B şıkkıdır.
+            Switch ifadesinde herbir case bloğunun sonunda bulunan break ifadesi isteğe bağlıdır.
+            Break ifadesi olmadan, switch ifadesi sonuna kadar gerçekleşir veya
+            bir break ifadesi ile karşılaşana kadar sonraki case etiketine devam eder.
+    */
+}
