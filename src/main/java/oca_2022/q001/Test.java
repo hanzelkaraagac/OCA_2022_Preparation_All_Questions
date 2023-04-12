a69b8cb207624ffdccb0fc3268cbccca8a9dccc9
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-12 15:31:14 +0300

    0CA 2022 Prepartion All Questions

---
+++ src/main/java/oca_2022/q001/Test.java
@@ -0,0 +1,15 @@
+package oca_2022.q001;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 1;	
+		if (x++ < ++y) { // x is still 1,   y is 2 ==> true
+			System.out.print("Hello ");
+		}else {
+			System.out.print("Welcome ");
+		}
+		System.out.print("Log " + x + ":" + y);  // x ix used again so x becomes 2		
+	}
+}
