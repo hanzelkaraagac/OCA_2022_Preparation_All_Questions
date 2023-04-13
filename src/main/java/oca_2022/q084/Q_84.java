f92040bf036f24b5cc9eab2d3e27dd737ef39170
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 18:48:09 +0300

    0CA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q084/Q_84.java
@@ -0,0 +1,21 @@
+package oca_2022.q084;
+
+public class Q_84 {
+//	public static void main(String[] args) {
+//
+	int array []=new int[] {1,2,3};            //A
+	int array2[] =new int [3]; 					//B
+//	array2[0]=1;
+//	array2[1]=2;
+//	array2[2]=3;
+//
+//	int array3[]=new int[] {1,2,3};			//C
+//
+//	int array4[]= new int[3]; array4= {1,2,3,5};	//D
+//  int array5[]=new int []{1,2,3};			//E
+//	}
+
+}
+
+//A C yanlis
+//B E
