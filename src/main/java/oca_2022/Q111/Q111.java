1c10c991b4fafe2b062fd08fb58a8bd1d4aaf940
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 20:17:32 +0300

    0CA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/Q111/Q111.java
@@ -0,0 +1,19 @@
+package oca_2022.Q111;
+
+public class Q111 {
+    public static void main(String[] args) {
+        int ii=0;
+        int jj=7;
+        for(ii=0; ii<jj-1; ii=ii+2){
+            System.out.println(ii+" ");
+        }
+    }
+}
+/*What is the result?
+A. 2 4
+B. 0 2 4 6
+C. 0 2 4
+D. Compilation fails
+Answer: C
+
+ */
