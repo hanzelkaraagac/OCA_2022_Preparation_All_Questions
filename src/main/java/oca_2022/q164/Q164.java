c3d224b902f8c2afa2448085c7987ab98aa32919
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 23:06:50 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q164/Q164.java
@@ -0,0 +1,15 @@
+package oca_2022.q164;
+
+import java.util.Arrays;
+
+public class Q164 {
+	
+	public static void main(String[] args) {
+		int [] intArr= {15,30,45,60,75};
+	intArr[2]=intArr[4];
+	intArr[4]=90;
+
+		System.out.println(Arrays.toString(intArr));
+
+}
+}
