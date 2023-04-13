b0584ddb0b79f88088b4bef9d38b7916c0cd2c78
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 22:17:32 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/Q118/App.java
@@ -0,0 +1,34 @@
+package oca_2022.Q118;
+
+public class App {
+    public static void main(String[] args) {
+        String str1="Java";
+        String str2=new String("java");
+        //line1
+        if(str2.equals(str1.toLowerCase()))
+        
+       
+        {
+            System.out.println("Equal");
+        }else{
+            System.out.println("Not Equal");
+        }
+    }
+}
+/*
+Which code fragment, when inserted at line n1, enables the App class to print Equal?
+ A)  Str1.toLowerCase();
+     if(str1=str2)
+  b if(str2.equals(str1.toLowerCase()))
+        {
+ C)  Str1.toLowerCase();
+     if(str1.equals(str2))
+ D)  if(str1.toLowerCase()==str2.toLowerCase())
+ A. Option A
+ B. Option B
+ C. Option C
+ D. Option D
+ Answer: B
+
+
+ */
