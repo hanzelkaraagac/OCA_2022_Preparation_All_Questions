f234beaa0c08cc0b16adc900c5a6501c6a9e822f
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 23:42:21 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q198/Q198.java
@@ -0,0 +1,38 @@
+package oca_2022.q198;
+
+public class Q198 {
+    /*
+     * SalesMan.Java:
+     *
+     * package sales;
+     * public class SalesMan{}
+     *
+     * Product.java:
+     *
+     * package sales.products;
+     * public class Product{}
+     *
+     * MArket.java:
+     *
+     * package market;
+     *  //code here
+
+     * public class USMarket{
+     * SalesMan sm;
+     * Product p;
+     *
+     */
+
+    /*
+     * Which line inserted in line 2 enables the code to compile?
+     *
+     * A.import sales.*
+     * B.import java.sales.products.*
+     * C.import sales
+     *   import sales.products
+     * D.import sales.*
+     *   import products.*
+     * E.import sales.*
+     *   import sales.products.*
+     */
+}
