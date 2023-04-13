c65e5ce5d72631a776de956c28acbc5df4b498a9
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 23:32:05 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q185p3/C.java
@@ -0,0 +1,12 @@
+package oca_2022.q185p3;
+//line n2
+import q185.A;
+import q185.p2.B;
+
+public class C {
+    public static void main(String[] args) {
+        A o1= new A();
+        B o2= new B();
+    }
+}
+
