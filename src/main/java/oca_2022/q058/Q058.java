3630bac73fe4e240b89fa45b9acfaeafe88fd982
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 18:31:14 +0300

    0CA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q058/Q058.java
@@ -0,0 +1,18 @@
+package oca_2022.q058;
+
+ class Q058 {
+	public static void main(String[] args) {
+		System.out.println("elif");
+		
+		
+	}
+
+}
+/**
+ 58. Which statement is true about the main() method?
+ A. It is invoked by JRE
+ B. It is a final method
+ C. It returns true if it is executed successfully at run time
+ D. It must be defined within a public class
+ Answer: A
+ */
