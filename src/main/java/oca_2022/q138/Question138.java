12d47978d3f5b2f8355d7936848be1d033232c32
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 22:29:33 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q138/Question138.java
@@ -0,0 +1,22 @@
+package oca_2022.q138;//package JavaVersion18;
+//
+//public class Question138 {
+
+
+//
+//		 Which two statement true about Java byte code?
+//				 
+//				 
+//		 A. It can be run any platform
+//		 B. It can run on any platform if it was compiled for that platform
+//		 C. It can run on any platform that has the Java Runtime Environment.
+//		 D. It can run on any platform that has a Java Compiler
+//		 E.It can run on any platform that has the Java Runtime Environment and Java Compiler
+//		 
+//		 
+//		 Answer: D
+//		 
+	
+
+	
+
