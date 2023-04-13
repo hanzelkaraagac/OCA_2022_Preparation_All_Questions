4506575a9d50fd9b98428ebe428c1a23c9266457
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 23:29:44 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q181/A.java
@@ -0,0 +1,15 @@
+package oca_2022.q181;
+
+public class A {
+    public void a(){}
+    int a;
+}
+
+/*
+A. Only the A.Java file compiles successfully.// Dogru Cevap
+B. Only the B.java file compiles successfully.
+C. Only the C.java file compiles successfully.
+D. The A.Java and B.java files compile successfully.
+E. The B.java and C.java files compile successfully.
+F. The A.Java and C.java files compile successfully.
+ */
---
+++ src/main/java/oca_2022/q181/B.java
@@ -0,0 +1,11 @@
+package oca_2022.q181;
+
+public class B {
+    public int doStuff(){
+        int x = 100; //Illegal modifier for parameter x; only final is permitted
+
+        return x;
+    }
+
+ //  }
+}
