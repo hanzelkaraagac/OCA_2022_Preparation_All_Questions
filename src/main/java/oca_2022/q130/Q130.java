84b6b622b2a633c3023dc9d3c01edc45636c643a
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 22:26:16 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q130/Q130.java
@@ -0,0 +1,14 @@
+package oca_2022.q130;
+
+public class Q130 {
+
+	public static void main(String[] args) {
+		System.out.println("5 + 2 = " + 3 + 4);
+		System.out.println("5 + 2 = " + (3 + 4));
+
+//Answer D.      5 + 2 = 34
+                 	//  5 + 2 = 7
+
+	}
+
+}
