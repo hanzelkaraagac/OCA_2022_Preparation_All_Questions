5fbe8f98c3f30565f6152d4d0586901f1bf87804
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 18:35:42 +0300

    0CA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q070/Q_70.java
@@ -0,0 +1,40 @@
+package oca_2022.q070;
+
+public class Q_70 {
+	public static void main(String[] args) {
+//        int x = 10;
+//         ++x;//11
+//        int z = 0;
+//
+//        if(z >= 10 | z<= ++x){
+//            z = x;
+//        }else {
+//            z = x++;
+//        }
+//        System.out.println(z);
+
+
+  }
+    public void top(){
+        int x = 10;
+        ++x;//11
+        int z = 0;
+
+        if(z >= 10 | z<= ++x){
+            z = x;
+        }else {
+            z = x++;
+        }
+        System.out.println(z);
+
+
+    }
+
+}
+/*
+What is the result?
+A. 11
+B. 10
+C. 12
+D. A compile time error occurs.
+ */
