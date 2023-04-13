87d06bcbe1073b134a3e7b079590ac411f7f8500
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 22:27:46 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q133/Question133.java
@@ -0,0 +1,26 @@
+package oca_2022.q133;
+
+public class Question133 {
+
+//package p1;
+	public class App {
+		public void main (String []args) {
+			
+		System.out.println("Java");	
+		
+		}
+	}}
+
+//	and of Test.java:
+//		
+//		package p1.p2;
+//	public class Test {}
+//	
+//	Which is true?
+//			
+//			A. The App.class file is stored within the p1 folder. The Test.class file is stored withi the p2 sub-folder of p1.
+//			B. The App class is accessible within the Test class without an importanrt statement.
+//			C. Import p1.App; is used to access the App class within the Test class.
+//			D. It is optional to have the package statement as the first line of class definitions.
+			
+			//Answer:C
