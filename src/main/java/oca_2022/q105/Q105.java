da04b8a09da31da145c9ccaeee4067dd675283df
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 20:12:21 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q105/Q105.java
@@ -0,0 +1,44 @@
+package oca_2022.q105;
+
+import java.time.LocalDate;
+
+public class Q105 {
+
+
+
+//Given the code fragment:
+
+public static void main(String[] args) {
+    LocalDate date = LocalDate.of(2012, 01,32);
+     date.plusDays(10);
+    System.out.println(date);
+
+}
+
+		/* What is
+		the result?
+		A . 2012-02-10
+		B . 2012-02-11
+		C.Compilation fails
+		D.A DateTimeException is thrown at runtime.
+
+Answer:
+
+*/
+
+/*
+Q_144
+
+Given the code fragment:
+public static void main (String [] args) {
+LocalDate date = LocalDate.of (2012, 01, 32);
+ date.plusDays (10);
+ System.out.println (date);
+		  What is the result?
+		  A. 2012-02-10
+		  B. 2012-02-11
+		  C. Compilation fails
+		  D. A DateTimeException is thrown at runtime.
+
+*/
+}
