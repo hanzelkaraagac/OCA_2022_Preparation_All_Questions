8a91b81248c8f04b8318ac86e0e55553640a94a4
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 20:12:39 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q106/Q106.java
@@ -0,0 +1,15 @@
+package oca_2022.q106;
+
+public class Q106 {
+
+	
+//	106. Which two statements are true? (Choose two.)
+//			A. Error class is unextendable. 
+//			B. Error class is extendable. 
+//			C. Error is a RuntimeException. 
+//			D. Error is an Exception. 
+//			E. Error is a Throwable. 
+//			
+//			Answer:
+//				B C
+}
