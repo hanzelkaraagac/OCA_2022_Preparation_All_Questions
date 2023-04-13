aa553a9768eb6d61ed44c89454ebe491e2779005
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 03:10:48 +0300

    0CA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q019/Q019.java
@@ -0,0 +1,39 @@
+package oca_2022.q019;
+
+public class Q019 {
+    /*
+
+     Which three statements are true about the structure of a Java class? (Choose three.)
+
+     A. A class cannot have the same name as its field.=
+     B. A public class must have a main method.
+     C. A class can have final static methods.
+     D. A class can have overloaded private constructors.
+     E. Fields need to be initialized before use.
+     F. Methods and fields are optional components of a class.
+
+
+
+
+    ANSWER:
+
+        The three statements that are true about the structure of a Java class are:
+
+        C. A class can have final static methods.
+        D. A class can have overloaded private constructors.
+        E. Fields need to be initialized before use.
+
+        Note that statement A is false, as a class can have a field with the same name as the class itself.
+        Statement B is also false, as a public class does not necessarily need to have a main method.
+        Finally, statement F is also false, as methods and fields are essential components of a Java class.
+
+        Java sınıfının yapısıyla ilgili üç doğru ifade şunlardır:
+
+        D. Bir sınıf, aşırı yüklenmiş özel yapıcıları olabilir.
+        E. Alanlar kullanımdan önce başlatılmalıdır.
+        F. Yöntemler ve alanlar bir sınıfın isteğe bağlı bileşenleridir.
+
+
+
+    */
+}
