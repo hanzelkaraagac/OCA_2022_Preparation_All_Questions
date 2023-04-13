e9b796981678d154c724beae046b4c81ce29a822
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 23:06:33 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q163/Q163.java
@@ -0,0 +1,15 @@
+package oca_2022.q163;
+
+public class Q163 {
+public static void main (String []args) {
+	int x=1;
+	int y=0;
+	if (x++>++y) {
+		System.out.println("Hello");
+	} else {
+		System.out.println("Welcome");
+		System.out.println("log"+x+":"+y);
+		
+	}
+}
+}
