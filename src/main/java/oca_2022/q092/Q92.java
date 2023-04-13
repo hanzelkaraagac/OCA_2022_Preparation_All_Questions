de526033a5a088ae4c4449490e512a690228fd84
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 18:56:29 +0300

    0CA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q092/Q92.java
@@ -0,0 +1,37 @@
+package oca_2022.q092;
+
+public class Q92 {
+
+
+    public static void main(String[] args) {
+        //line n1
+//    	String x = "1";
+//    	Integer x = new Integer("1");
+        byte x = 1;
+        switch (x) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+        }
+
+    }
+}
+//CIBSS  ==> Char, Integer, Byte, String, Short u switch te kullanabiliriz
+// Boolean, long, float, double we can not use with
+
+/*
+*Which three code fragments can be independently inserted at line n1 to enable the code to print One?
+(Choose three.)
+A. byte x = 1;
+B. short x = 1;
+C. String x = "1";
+D. long x = 1;
+E. double x = 1;
+F. Integer x = new Integer("1");
+Answer: ABF
+*  */
+
+
