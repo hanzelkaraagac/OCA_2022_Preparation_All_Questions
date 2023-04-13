38ebf48e31a13a04effac75f835c9a5418f58235
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 23:30:57 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q183/Q183.java
@@ -0,0 +1,15 @@
+package oca_2022.q183;
+
+public class Q183 {
+    /*
+    *
+*
+183. Which statement is true about the switch statement?
+A. It must contain the default section.
+B. The break statement, at the end of each case block, is mandatory. C. Its case label literals can be changed at runtime.
+D. Its expression must evaluate to a single value.
+Answer: D
+    * */
+}
+
+
