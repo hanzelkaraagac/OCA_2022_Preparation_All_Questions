561c22f8cf3d5ec69fad071b3a947d4d79f1b7d6
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 22:44:39 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q144/Question144.java
@@ -0,0 +1,17 @@
+package oca_2022.q144;
+
+public class Question144 {  //public class Greeting
+	
+	public static void main(String[]args) {
+		System.out.println("Hello " + args[0]);
+
+		
+	}
+}
+
+
+//
+//Which set of commnds prints Hello Duke in the console?
+//		
+//		Answer: javac Greeting.java
+//		        java Greeting Duke
