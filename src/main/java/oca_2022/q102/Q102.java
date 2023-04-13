a41b14360e90032fcc5f5dd5785b9baa59dfbaff
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 20:11:13 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q102/Q102.java
@@ -0,0 +1,28 @@
+package oca_2022.q102;
+
+public class Q102 {
+
+	
+	//What is the output?
+    public static void main(String[] args) {
+        String[] planets ={"Mercury", "Venus", "Earth", "Mars"};
+
+        System.out.print(planets.length);
+        System.out.print(planets[1].length());
+    }
+    //ANSWER IS : E. 4 5
+
+
+}
+
+/*
+/ What is the output?
+A. 44
+B. 35
+C. 47
+D. 54
+E. 45
+F. 421
+Answer: E
+
+*/
