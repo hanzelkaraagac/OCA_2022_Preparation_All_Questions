a6c27f2e8c225294a1750da06c4062ee12917590
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 22:22:48 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q125/Q125.java
@@ -0,0 +1,34 @@
+package oca_2022.q125;
+
+public class Q125 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+////A.
+//	public static void main(String[] args) { //Answer A
+//		System.out.println("I float. ");
+//	}
+//
+//	// B.
+//	public class Cake {
+//		public static void main(String[] ) {  //args olmadigi icin eksik 
+//			System.out.println("Chocolate");
+//	}
+// //C.
+// public class Dog {
+//	public void main(String[] args) { // static olmadigi icin eksik
+//			System.out.println("Squirrel");
+//		}
+//	//D.
+//	public class Bank {
+//		public static void main(String () args) { // [] yerine () oldugu icin yanlis
+//			System.out.println("Earn interest. ");
+//		}
+//	}
+//}
+
