2cd586798590c88237d9da76d023cb229850d995
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 16:37:05 +0300

    tion Section All Questions and Answers

---
+++ src/main/java/oca_2022/q035/Q035.java
@@ -0,0 +1,22 @@
+package oca_2022.q035;
+
+public class Q035 {                           //cibss=== char int byte short string sadece kullanilir
+	public static void main(String[] args) {
+//		boolean opt = true;
+//	switch (opt) { // switch does not work with  long, double, float and boolean
+//		case true:
+//			System.out.println("True");
+//			break;
+//		default:
+//			System.out.println("***");
+//		}
+//		System.out.println("Done");
+}
+}
+
+//Which modification enables the code fragment to print TrueDone?
+//A.	Replace line 5 With String opt = "true";Replace line 7 with case "true":
+//B.	Replace line 5 with boolean opt = l;Replace line 7 with case 1:
+//C.	At line 9, remove the break statement.
+//D.	Remove the default section.
+
