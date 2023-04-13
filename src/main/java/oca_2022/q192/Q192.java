5da44fe167ea854dc0467048b34d91dbca82deaa
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 23:40:38 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q192/Q192.java
@@ -0,0 +1,23 @@
+package oca_2022.q192;
+
+public class Q192 {
+    int[] array = {1,2,3,4,5};
+}
+
+// *** enhanced loop can walk through the elements of array in order ***
+
+/*
+* Given the code fragment:
+* And given the requirements:
+* 1. Process all the elements of the array in the order of entry.
+* 2. Process all the elements of the array in the reverse order of entry.
+* 3. Process alternating elements of the array in the order of entry. 
+* 		Which two statements are true? (Choose two.)
+*
+*
+A. Requirements 1, 2, and 3 can be implemented by using the enhanced for loop.
+B. Requirements 1, 2, and 3 can be implemented by using the standard for loop.        √
+C. Requirements 2 and 3 CANNOT be implemented by using the standard for loop.
+D. Requirement 1 can be implemented by using the enhanced for loop.						√
+E. Requirement 3 CANNOT be implemented by using either the enhanced for loop or the standard for loop.
+* */
