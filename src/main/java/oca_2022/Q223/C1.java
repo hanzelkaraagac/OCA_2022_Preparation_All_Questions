a1159d345219d7c56007aa32d22f144a42240103
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 23:52:14 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/Q223/C1.java
@@ -0,0 +1,43 @@
+package oca_2022.Q223;
+
+public class C1 {
+	
+}
+
+
+	class C2 extends C1 {
+		
+	}
+
+	class C3 extends C2 {
+		public static void main(String[] args) {
+			//int array[] = new int[3]; array = {1, 2, 3};
+			
+//			int array2[] = new int[] {1,2,3};
+//			
+//			int array1[] = new int[] {1,2,3};
+//			
+//			int array3[] = new int[3] {1, 2, 3};
+//			
+//			int array5[] = new int[]; array5 = {1, 2, 3};
+//			
+//			
+//			int array[] = new int[3]; //dogru
+//			array[0] = 1; array[1] = 2;
+//			array[2] = 3;
+			
+	
+
+	//and the code fragment:
+
+	C1 obj1 = (C1) new C2();
+
+	C2 obj2 = (C2) new C3();
+
+	C2 obj3 = (C2) new C1(); //cant cast
+
+	C3 obj4 = (C3) obj2;
+
+
+
+}}
