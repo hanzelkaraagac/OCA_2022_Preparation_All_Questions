7478ac1c669fc2a0f3dfad714ee20c28af8bf517
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 22:46:38 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q148/Question148.java
@@ -0,0 +1,34 @@
+package q148;
+
+public class Question148 {//150ileyer degistirmis
+
+
+
+    public static void main(String[] args) {
+        String arg1 = args[0];
+        String arg2 = args[1];
+        String arg3 = args[2];
+        System.out.println("Arg is "+ arg3);
+    }
+}
+// and this output:
+//              Arg is 2
+
+//Which command should you run to obtain this output?
+//A.	java MyFile 2
+//B.	java MyFile 1 2 3 4
+//C.	java MyFile 1 2 2
+//D.	java MyFile 2 2
+//Answer: C
+
+//second version
+
+//public static void main(String[] args) {
+//    String arg1 = args[1];
+//    String arg2 = args[2];
+//    String arg3 = args[3];
+//    System.out.println("Arg is "+ arg3);
+//}
+
+
+//Answer javaMyFile 1 3 2 2
