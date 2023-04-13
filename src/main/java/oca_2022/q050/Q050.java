3c6fd17934ea7d6b760f9fee1482cfc07137ca0e
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 18:27:24 +0300

    0CA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q050/Q050.java
@@ -0,0 +1,16 @@
+package oca_2022.q050;
+
+public class Q050 {
+//  Which two code fragments cause compilation errors? (Choose two.)
+	public static void main(String[] args) {
+//		double y1 = 203.22; float fit = y1; //Type mismatch: cannot convert from double to float
+//
+//		float fit1 = (float) 1_11.0;
+//
+//		float fit2  =  100.00; 						// Type mismatch: cannot convert from double to Float
+//
+//		int y2 = 100; float fit3= (float)y2;
+//
+//		float fit4 = 100.00F;
+	}
+}
