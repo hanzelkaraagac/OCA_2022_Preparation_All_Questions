abcef19383f23a80340f0c9c733a853b57ff63b2
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 22:18:42 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q121/MainTest.java
@@ -0,0 +1,16 @@
+package q121;
+
+public class MainTest {
+
+//	public static void main(int[] args) {
+//		System.out.println("int main " + args[0]);
+//	}
+//	public static void main(Object[] args) {
+//		System.out.println("Object main " + args[0]);
+//	}
+	public static void main(String[] args) {
+		System.out.println("String main " + args[0]);
+
+	}
+
+}
