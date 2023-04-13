9c16151b3e7994ffe6028b769eb320feaf3136f2
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 23:30:17 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q181_1/A.java
@@ -0,0 +1,42 @@
+
+package oca_2022.q181_1;
+
+//import java.util.ArrayList;
+//
+//// package name must be
+//
+//class A {
+//	 public void a(){}
+//	    int a;
+//	}
+
+//public class B {
+//    private int doStuff(){
+//       int x = 100; //(basinda private olmaz)
+//        //local variable da access modfier kullanamayiz
+//        return x++;
+//    }
+//    List list = new ArrayList<>();
+//}
+//
+//
+//
+
+import java.io.IOException;
+
+//	package p1;
+	class A {
+	    public void main (String fileName) throws IOException {}
+	}
+
+
+
+
+	/*
+	A. Only the A.Java file compiles successfully.// Dogru Cevap
+	B. Only the B.java file compiles successfully.
+	C. Only the C.java file compiles successfully.
+	D. The A.Java and B.java files compile successfully.
+	E. The B.java and C.java files compile successfully.
+	F. The A.Java and C.java files compile successfully.
+	 */
