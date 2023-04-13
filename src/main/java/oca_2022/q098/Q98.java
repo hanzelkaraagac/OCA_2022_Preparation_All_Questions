69dbb202296c1694c7f0d462bf724596fff52943
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 20:09:44 +0300

    0CA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q098/Q98.java
@@ -0,0 +1,35 @@
+package oca_2022.q098;
+
+public class Q98 {
+
+}
+class MyFile {
+    public static void main(String[] args) {
+        String arg1= args[1];
+        String arg2= args[2];
+        String arg3= args[3];
+
+
+        System.out.println("Arg is " + arg3);
+    }
+}
+/*
+Which command-line arguments should you pass to the program to obtain the following output? Arg is 2
+
+A. java MyFile 1 3 2 2
+B. java MyFile 2 2 2
+C. java MyFile 0 1 2 3
+
+Answer: A
+ */
+// 2nd version
+/*
+Soru ayni yalniz cevap siklari farkli
+
+A. java MyFile 1 3 2 2
+B. java MyFile 2 2 2
+C. java MyFile 1 2 2 3 4
+D.  java MyFile 0 1 2 3
+
+Answer: D
+ */
