02928a6e87be5597d4020380dd2b23cd1890718a
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 18:32:43 +0300

    0CA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q061/Q_61.java
@@ -0,0 +1,22 @@
+package oca_2022.q061;
+
+public class Q_61 {
+	
+	int []array= {1, 2, 3, 4, 5,};
+	
+	
+	/**
+	 * And given the requirements:
+* 1. Process all the elements of the array in the reverse order of entry.
+* 2. Process all the elements of the array in the order of entry.
+* 3. Process alternating elements of the array in the order of entry. Which two statements are true? (Choose two.)
+
+A. Requirements 1, 2, and 3 can be implemented by using the enhanced for loop.
+B. Requirements 1, 2, and 3 can be implemented by using the standard for loop.
+C. Requirements 2 and 3 CANNOT be implemented by using the standard for loop.
+D. Requirement 2 can be implemented by using the enhanced for loop.
+E. Requirement 3 CANNOT be implemented by using either the enhanced for loop or the standard for loop.
+Answer: B C
+	 */
+//I think answer BD
+}
