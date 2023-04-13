363939622ea97dd7b2e1262961c58b09d2144403
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 22:42:37 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q140/Question140.java
@@ -0,0 +1,39 @@
+package oca_2022.q140;
+
+public class Question140 {
+
+	
+	    public static void main(String[] args) {
+	        int iVar = 100;
+	        float fVar = 100.100f;
+	        double dVar = 123;
+	          fVar = iVar;//kucuk buyuge olur
+//          iVar = fVar; //buyuk kuvcuge olmaz
+//	          fVar = dVar;
+//         dVar = fVar;
+//          iVar = dVar;
+//	          dVar = iVar;
+
+	          /*
+	        Second Version
+	        the question was same but number of lines was changed.
+	        ivar=fvar was at line 8.
+	        fvar=dvar at line 9.
+	        ivar= dvar at line 11.
+	        be careful at line numbers.
+	         */
+
+	        /* which three lines fail to compile? (choose three.)
+	        A. Line 7
+	        B. Line 8
+	        C. Line 9
+	        D. Line 10
+	        E. Line 11
+	        F. Line 12
+
+	        Answer: ADF
+	         */
+
+
+	    }
+	}
