1091de11af34ba45157e6817465191b5af2cd799
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 23:45:05 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/Q206/Q206.java
@@ -0,0 +1,28 @@
+package oca_2022.Q206;
+
+public class Q206 {
+    //given code fragment
+	public static void main(String[] args) {
+		
+	
+    int a[]={1,2,3,4,5};
+    //for(XXX) 
+    for(int e=0; e<5; e+=2){
+        System.out.println(a[e]);
+    }
+	}
+}
+/*
+which option can replace xxx to enable the code to print 135?
+A  int e=0; e<=4; e++
+B  int e=0; e<5; e+=2
+C  int e=1; e<=5; e+=1
+D  int e=1; e<5; e+=2
+
+A. option A
+B. option B
+C. option C
+D. option D
+
+answer b
+ */
