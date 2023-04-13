1b3f7f52328b5caf2bb164e31d875ec1f1da498e
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 23:06:09 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q161/q161.java
@@ -0,0 +1,15 @@
+package oca_2022.q161;
+
+public class q161 {
+
+	public static void main(String[] args) {
+//		Boolean  available ="TRUE";
+//		String tmAuthor=author,author="MC Donald";
+		Double price =200D;
+		Integer pages =20;
+		
+		
+
+	}
+
+}
