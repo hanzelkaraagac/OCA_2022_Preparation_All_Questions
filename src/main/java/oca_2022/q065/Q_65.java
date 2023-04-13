36342c2ec4d75944b15718e9d5c3c6971f44a8d1
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 18:33:57 +0300

    0CA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q065/Q_65.java
@@ -0,0 +1,20 @@
+package oca_2022.q065;
+
+public class Q_65 {
+	public static void main(String[] args) {
+        float var1 = (12_345.01 <= 123_45.00)? 12_456 : 124_56.02f;
+        float var2 = var1 + 1024;
+        System.out.print(var2);
+    }
+
+}
+//Answer D
+/**
+ * What is the result?
+A. An exception is thrown at runtime.
+B. Compilation fails.
+C. 13480.0
+D. 13480.02
+Answer: C
+ */
+ 
