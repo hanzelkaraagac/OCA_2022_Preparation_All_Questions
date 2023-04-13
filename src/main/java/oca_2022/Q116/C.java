2f220ac0ebee1a465eb609d1f782363040b93b24
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 22:15:53 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/Q116/C.java
@@ -0,0 +1,35 @@
+package oca_2022.Q116;
+class A{
+    public void test(){
+        System.out.println("A");
+    }
+}
+class B extends A{
+    public void test(){
+        System.out.println("B");
+    }
+}
+public class C extends A {
+    public void test() {
+        System.out.println("C");
+    }
+
+    public static void main(String[] args) {
+        A b1=new A();
+        A b2=new C();
+
+        b1=(A)b2;      //line1
+        A b3= b2;   //line2 C cannot be cast to Q116.B
+        b1.test();
+        b3.test();
+    }
+}
+/*What is the result?
+A. AB
+B. AC
+C. CC
+D. A ClassCastException is thrown only at line n1.
+E. A ClassCastException is thrown only at line n2.
+Answer: e
+
+ */
