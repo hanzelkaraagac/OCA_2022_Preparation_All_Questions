cf048930b286ee74a0aca892e6204763179eab1c
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 23:03:06 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q153/Question153.java
@@ -0,0 +1,31 @@
+package oca_2022.q153;
+public class Question153 {
+
+
+	//public	class Fieldint{
+			char c;
+			Character c2;
+			boolean b;
+			Boolean b2;
+			float f;
+			Float f2;
+			void printAll() {
+				System.out.println("c= "+c);//c=
+				System.out.println("c2= "+c2);//c2=
+				System.out.println("b= "+b);//b=false
+				System.out.println("b2= "+b2);//b2=false
+				System.out.println("f= "+f);//f=0.0
+				System.out.println("f2= "+f2);//f2=0.0
+			}
+			
+		
+
+	//	public class Q147 {
+			
+			
+			public static void main(String[] args) {
+				Question153 f=new Question153();
+				f.printAll();
+			}
+
+		}
