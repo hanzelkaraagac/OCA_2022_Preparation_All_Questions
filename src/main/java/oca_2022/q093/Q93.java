8d90763705c303334a3655503d93d05823afd100
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 18:57:38 +0300

    0CA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q093/Q93.java
@@ -0,0 +1,38 @@
+package oca_2022.q093;
+
+public class Q93 {
+}
+	class Product{
+	int id;
+	String name;
+	
+	public  Product(int id, String name) { //void soruda yok
+		this.id = id;
+		this.name = name;
+	}
+
+	
+    public static void main(String[] args) {
+		
+	
+ //And given the code fragment:
+	Product p1 = new Product (101, "Pen");
+	Product p2 = new Product (101, "Pen");
+	Product p3 = p1;
+	boolean ans1 = p1==p2;
+	boolean ans2 = p1.name.contentEquals(p2.name);
+	System.out.println(ans1 + ":"+  ans2);
+	
+    }
+}
+	/*
+	 * What is the result?
+		A. true:true
+		B. true:false
+		C. false:true
+		D. false:false
+		
+		Answer: C
+	 */
+	
+	
