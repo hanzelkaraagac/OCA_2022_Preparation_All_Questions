769046169ff0362b7affddc21ba1dbaf6672f557
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 23:28:35 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q178/Q178.java
@@ -0,0 +1,14 @@
+package oca_2022.q178;
+
+public class Q178 {
+
+}
+/*
+178. Which is true about the switch statement?
+A. Its expression can evaluate to a collection of values.
+B. The break statement, at the end of each case block, is optional.
+C. Its case label literals can be changed at runtime.
+D. It must contain the default section.
+Answer: C  (I think) 
+
+*/
