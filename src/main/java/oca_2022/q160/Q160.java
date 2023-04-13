acfd7c42edad137a9880066dc9098c9a7ba9080a
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 23:05:20 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q160/Q160.java
@@ -0,0 +1,21 @@
+package oca_2022.q160;
+
+public class Q160 {
+
+    public static void main(String[] args) {
+	int	x=3;
+//		do {
+//			System.out.print("*");
+//			x--;
+//		}while (x>=0);
+//	}
+
+
+// * E dogru
+        x = 0;
+        do {
+            System.out.print("*");
+        } while (x++ < 3);
+    }
+
+}
