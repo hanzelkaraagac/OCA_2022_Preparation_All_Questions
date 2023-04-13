e7b46eef2f51c212164c92ee6a0af51cf6e0c742
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 23:44:05 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q203/Q203.java
@@ -0,0 +1,17 @@
+package oca_2022.q203;
+
+public class Q203 {
+    public static void main(String[] args) {
+        String myStr = "Hello World ";
+        myStr.trim();
+        int i1 = myStr.indexOf(" ");
+        System.out.println(i1);
+    }
+}
+/*
+* What is the result?
+A. An exception is thrown at runtime. 
+B. -1
+C. 5
+D. 10
+Answer: c*/
