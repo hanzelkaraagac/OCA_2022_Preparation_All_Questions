be798f216815e0ccfa347365e425d9f0f8c9b641
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 23:19:07 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q173/Q173.java
@@ -0,0 +1,21 @@
+package oca_2022.q173;
+
+public class Q173 {
+
+}
+/*
+ 173. Given these requirements:   
+Bus and Boat are Vehicle type classes.
+The start() and stop() methods perform common operations across the Vehicle class type. The ride() method performs a unique operations for each type of Vehicle. 
+Which set of actions meets the requirements with optimized code?
+A. * 1. Create an abstract class Vehicle by defining start() and stop() methods, and declaring the ride() abstract method.
+* 2. Create Bus and Boat classes by inheriting the Vehicle class and overriding the ride() method.
+B. * 1. Create an interface Vehicle by defining start() and stop() methods, and declaring the ride() abstract method.
+* 2. Create Bus and Boat classes by implementing the Vehicle class.
+C. * 1. Create an abstract class Vehicle by declaring stop(), start(), and ride() abstract methods.
+* 2. Create Bus and Boat classes by inheriting the Vehicle class and overriding all the methods.
+D. * 1. Create an interface Vehicle by defining default stop(), start(), and ride() methods.
+* 2. Create Bus and Boat classes by implementing the Vehicle interface and overriding the ride() method. Answer: A (I Think)
+
+ * 
+ */
