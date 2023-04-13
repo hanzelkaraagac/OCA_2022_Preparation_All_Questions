d7e419c380e41d502c7361720079a4a1ca71a5b5
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 22:25:56 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q129/Q129.java
@@ -0,0 +1,22 @@
+package oca_2022.q129;
+
+//public class Q129 {
+
+	//static int ans; //static yapilirsa ans class a ait olg icin ok boyle olursa cevap Answer =0;
+//		public static void main(String[] args) {
+
+//			try {
+//				int num =10;
+//				int div =0;
+//				int ans = num/div;
+//			}catch (ArithmeticException ae) {
+//				ans = 0;                     //line n1
+////			}catch (Exception e) {
+////				System.out.println("Invalid calculation");
+//			}
+//			System.out.println("Answer = " + ans);  //line n2
+//
+////			Answer E. Compilation fails at line n1 and line2.
+//		}
+
+//	}
