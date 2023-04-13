1d7f6a3d152f7eb5b0c3ef9b050f0a674d44fe4d
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 20:16:55 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/Q110/Base.java
@@ -0,0 +1,39 @@
+package oca_2022.Q110;
+//bunu kontrol edelim package leri mi ayri anlamadim
+public class Base {
+    public void test(){
+        System.out.println("Base ");
+    }
+}
+class DerivedA extends Base{
+    public void test(){
+        System.out.println("DerivedA ");
+    }
+}
+class DerivedB extends  DerivedA{
+    public void test(){
+        System.out.println("DerivedB ");
+    }
+
+    public static void main(String[] args) {
+        Base b1=new DerivedB();
+        Base b2=new DerivedA();
+        Base b3=new DerivedB();
+        b1=b3;
+        Base b4= b3;
+        b1.test();
+        b4.test();
+
+    }
+}
+/*What is the result?
+
+A. BaseDerivedA
+B. BaseDerivedB
+C. DerivedBDerivedB
+D. DerivedBDerivedA
+E. A ClassCastException is thrown at runtime.
+
+Answer: C
+
+ */
