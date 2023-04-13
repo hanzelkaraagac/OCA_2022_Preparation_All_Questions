2f355f43b963b3f10963baffbb72af1b538a9b6d
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 17:25:20 +0300

    0CA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q048/Test.java
@@ -0,0 +1,21 @@
+package oca_2022.q048;
+
+public class Test {
+	//public static void main(String[] args) {
+	
+		// line n1
+//	Test() {}
+//	String str = "Java";
+	
+	//for(int iVal = 0; iVal <=5; iVal++){//main method olmasi gerek
+		
+	}
+//}
+
+
+	//Which two code fragments can be inserted at line n1?
+	//A.	String str = "Java";
+	//B.	for(int iVal = 0; iVal <=5; iVal++){}
+	//C.	Test() {}
+	//D.	package p1;
+	//E.	import java.io.*;
