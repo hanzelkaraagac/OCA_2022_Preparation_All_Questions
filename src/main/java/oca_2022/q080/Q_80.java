a680ef1d4e494303672558b071c9d152a16f73e1
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 18:39:19 +0300

    0CA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q080/Q_80.java
@@ -0,0 +1,16 @@
+package oca_2022.q080;
+
+public class Q_80 {
+	/**
+	 * 80. Which two features can be implemented in a Java application by encapsulating the entity classes used?
+(Choose two.)
+
+A. data validation
+B. compile time polymorphism
+C. data hiding
+D. data abstraction
+E. data memory optimization
+Answer: C D
+	 */
+
+}
