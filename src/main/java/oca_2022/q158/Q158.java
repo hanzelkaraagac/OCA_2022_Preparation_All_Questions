313b84f93795fb61bca3f2420b5885c4ac0c06b4
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 23:04:33 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q158/Q158.java
@@ -0,0 +1,18 @@
+package oca_2022.q158;
+
+public class Q158 {
+	public static void main (String[]args) {
+		int aVar=9;
+		if (aVar++ <10) {
+			System.out.println(aVar +"Hello Universe!");
+		}else {
+			System.out.println(aVar +"Hello World!");
+				
+			}
+			
+		}
+
+
+	}
+
+
