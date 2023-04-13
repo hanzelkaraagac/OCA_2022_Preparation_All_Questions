1b0ab5e0bfd8bfa20dd2b2a7e6d117e3a0e0922c
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 23:17:49 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q171/Q171.java
@@ -0,0 +1,16 @@
+package oca_2022.q171;
+
+public class Q171 {
+
+}
+/*
+ * 
+ 171. Which three statements are true about exception handling? (Choose three.) A. Only unchecked exceptions can be rethrown.
+B. All subclasses of the RuntimeException class are not recoverable.
+C. The parameter in a catch block is of Throwable type. 
+D. All subclasses of the RuntimeException class must be caught or declared to be thrown. 
+E. All subclasses of the RuntimeException class are unchecked exceptions.
+F. All subclasses of the Error class are not recoverable. 
+//Answer CEF (I Think)
+
+ */
