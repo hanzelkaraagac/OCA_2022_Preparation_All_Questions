9bac2a9e3501d832be257abf402b6a56fdbb2723
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 18:52:34 +0300

    0CA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q089/Q89.java
@@ -0,0 +1,16 @@
+package oca_2022.q089;
+
+public class Q89 {
+
+	
+	/*What is the name of the Java concept that uses access modifiers to protect variables and hide them within a class?
+A.	Encapsulation
+B.	Inheritance
+C.	Abstraction
+D.	Instantiation
+E.	Polymorphism
+  
+  Answer: A
+	 * 
+	 */
+}
