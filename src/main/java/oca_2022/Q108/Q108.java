f456030628d8cff587f616e621f1e469b1232902
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 20:16:07 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/Q108/Q108.java
@@ -0,0 +1,23 @@
+package oca_2022.Q108;
+
+public class Q108 {
+    public static void main(String[] args) {
+        String[] arr={"A","B","C","D"};
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.println(arr[i]+" ");
+            if(arr[i].equals("D")){//if condtion true ozman workdone yazdirir ve cikar
+                System.out.println("Work done");
+                break;
+            }
+            continue;
+        }
+    }
+}
+/*
+What is the result?
+A. A B C Work done
+B. A B C D Work done
+C. A Work done
+D. Compilation fails
+Answer: B
+ */
