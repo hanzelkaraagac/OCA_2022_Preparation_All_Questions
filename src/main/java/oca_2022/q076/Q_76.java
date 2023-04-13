481244d52341c0f48cfeda0bf72be6b2938c40d0
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 18:37:51 +0300

    0CA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q076/Q_76.java
@@ -0,0 +1,28 @@
+package oca_2022.q076;
+
+public class Q_76 {
+
+}
+abstract class Planet{
+    protected void revolve(){
+
+    }
+    abstract void rotate();
+}
+class Earth extends Planet{
+    public void revolve(){ //protected
+
+    }
+    protected void rotate(){ //make a protected
+
+    }
+}
+/*
+Which two modifications enable the code to compile? or question might be three modifications
+A. Make the method at line 8 protected.
+B.	Make the method at line 8 public.
+C.	Make the method at line 10 protected.
+D.	Make the method at line 4 public.
+E.	Make the method at line 2 public.
+Answer: AC veya BC ikiside calisir
+ */
