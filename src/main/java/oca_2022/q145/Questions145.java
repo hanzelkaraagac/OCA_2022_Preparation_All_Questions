037e384ae0ad9cca692385b3769c4cff878007a5
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 22:45:00 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q145/Questions145.java
@@ -0,0 +1,65 @@
+package oca_2022.q145;
+
+public class Questions145 { //Class name is SumTest
+	
+	
+	    public static void doSum(Integer x, Integer y) {
+	        System.out.println("Integer sum is " + (x + y));
+	    }
+
+	    public static void doSum(double x, double y) {
+	        System.out.println("double sum is " + (x + y));
+	    }
+
+	    public static void doSum(float x, float y) {
+	        System.out.println("float sum is " + (x + y));
+	    }
+
+	    public static void main(String[] args) {  //once primitive cagrilir
+
+	        doSum(10, 20);
+	        doSum(10.0, 20.0);
+
+	    }}
+	    
+	    
+    
+//	    Answer: 
+//	    	float sum is 30.0
+//	    double sum is 30.0
+//	    	
+//	    	
+//	    /*
+//	      //      Second Version
+//	     public static void doSum(Integer x, Integer y) {
+//	        System.out.println("Integer sum is " + (x + y));
+//	    }
+//	    public static void doSum(double x, double y) {
+//	        System.out.println("double sum is " + (x + y));
+//	    }
+//
+//	    public static void doSum(float x, float y) {
+//	        System.out.println("float sum is " + (x + y));
+//	    }
+//
+//	    public static void doSum(int x, int y) {
+//	        System.out.println("int sum is " + (x + y));
+//	    }
+//
+//	    public static void main(String[] args) {  //once primitive cagrilir
+//	        doSum(10, 20);
+//	        doSum(10.0, 20.0);
+//
+//	     */
+//
+//
+//
+//	}
+//
+//	// What is the result?
+//	//A.
+//	// Option A
+//	//float sum is 30.0
+//	//double sum is 30.0
+//
+//
