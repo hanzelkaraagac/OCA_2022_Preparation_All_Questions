f175f5ab08497a79311289e81056520cd6a3db07
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 23:04:10 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q156/Q156.java
@@ -0,0 +1,21 @@
+package oca_2022.q156;
+
+public class Q156 {
+
+	public static void main(String[] args) {
+		// Which statement best describe encapsulation?
+		//Encapsulation ensures that class can be designed so that only certain fields and methods of
+		//an object are acesible from other objects....
+	//157
+		int num [][]=new int [3] [1]	;
+		for (int i=0;i<num.length;i++) {
+			for(int j=0;j<num[i].length;j++){
+				num [i][j]=10;
+
+				
+			}
+	}
+
+	}
+}
+
