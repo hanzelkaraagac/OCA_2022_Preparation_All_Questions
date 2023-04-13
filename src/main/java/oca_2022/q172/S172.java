33171d2318def4247d94933b5355e1a499beb53a
hanzelkaraagac <hanzelgamess@gmail.com>
2023-04-13 23:18:17 +0300

    OCA 2022 Preparation Section All Questions and Answers

---
+++ src/main/java/oca_2022/q172/S172.java
@@ -0,0 +1,14 @@
+package oca_2022.q172;
+
+public class S172 {
+
+	public static void main(String[] args) {
+	
+int num = 5;
+do {   //--num olursa cevap--> 4 print eder
+System.out.print(num-- +" "); //num-- olursa cevap--> 5 print 
+}while (num==0); //num==0 --> 5 print eder,
+                 //num!=0 --> 54321 print ediyor                 
+	}
+
+}
